9b51e8a19d98cb27a23c12c1dd94432b3a5dc4e7
MehmetZalah <MehmetZalah@gmail.com>
2018-09-20 13:29:11 +0200

    Add two another sensors

--- mehmet-alarm.ino
+++ mehmet-alarm.ino
@@ -1,19 +1,37 @@
-//define fire pins
-#define powerfire A3
-#define gndfire A4
-#define isfire A2
+//define fire1 pins
+#define powerfire1 A3
+#define gndfire1 A4
+#define isfire1 A2
+
+//define fire2 pins
+#define isfire2 A5
+
+//define fire3 pins
+#define powerfire3 13
+#define gndfire3 A1
+#define isfire3 A0
 
 //define fire output
 #define gofire 4
 
 void setup() {
 
-              //fire initialization
-              pinMode(gndfire, OUTPUT);
-              digitalWrite(gndfire, LOW);
-              pinMode(powerfire, OUTPUT);
-              digitalWrite(powerfire, HIGH);
-              pinMode(isfire, INPUT);
+              //fire 1 initialization
+              pinMode(gndfire1, OUTPUT);
+              digitalWrite(gndfire1, LOW);
+              pinMode(powerfire1, OUTPUT);
+              digitalWrite(powerfire1, HIGH);
+              pinMode(isfire1, INPUT);
+
+              //fire 2 initialization
+              pinMode(isfire2, INPUT);
+
+              //fire 3 initialization
+              pinMode(gndfire3, OUTPUT);
+              digitalWrite(gndfire3, LOW);
+              pinMode(powerfire3, OUTPUT);
+              digitalWrite(powerfire3, HIGH);
+              pinMode(isfire3, INPUT);
 
               //fire Output
               pinMode(gofire, OUTPUT);
@@ -28,12 +46,13 @@ void loop() {
 
 void Mehmetfire(){
 
-              if(digitalRead(isfire)==HIGH){
+
+              if(digitalRead(isfire1)==HIGH && digitalRead(isfire2)==HIGH && digitalRead(isfire3)==HIGH){
 
                   digitalWrite(gofire, LOW);
               }
 
-              if(digitalRead(isfire)==LOW){
+              if(digitalRead(isfire1)==LOW || digitalRead(isfire2)==LOW || digitalRead(isfire3)==LOW){
 
                   digitalWrite(gofire, HIGH);
               }
